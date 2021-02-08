27912ff45c6162d100525feab14d0ddae3a8a17d
dawid <dawid.cieslak@mysza.software>
2021-02-08 21:03:26 +0100

    lab2 aiz

--- Algorytmy_i_zlożoność/Lab2/src/aiz/list/ListOne.java
+++ Algorytmy_i_zlożoność/Lab2/src/aiz/list/ListOne.java
@@ -1,65 +1,187 @@
 package aiz.list;
 
 
-import java.util.ArrayList;
-import java.util.List;
+public class ListOne<T> implements IList<T>{
 
-public class ListOne implements IList<Object>{
-
-    private List<Object> list = new ArrayList<>();
-    private int next = 0;
+    private ElemOne<T> first, last;
 
 
     @Override
-    public void addFirst(Object newData) {
-        this.list[next] = newData;
-        this.next = this.next + 1;
+    public void addFirst(T newData) {
+        ElemOne<T> old;
+        if(first != null){
+            old = first;
+            first = new ElemOne(newData, old);
+        }
+        else {
+            first = new ElemOne(newData);
+            last = first;
+        }
     }
 
     @Override
-    public void addLast(Object newData) {
+    public void addLast(T newData) {
+        if (first != null){
+            last.setNext(new ElemOne(newData));
+            last = last.getNext();
+        }
+        else {
+            first = new ElemOne(newData);
+            last = first;
+        }
 
     }
 
     @Override
-    public void addAtPosition(Object newData, int position) throws ListException {
-
+    public void addAtPosition(T newData, int position) throws ListException {
+        ElemOne<T> temp, temp2;
+        temp = first;
+        int size = this.size();
+        if (position>=0 && position<size){
+            for (int i=0; i<size;i++){
+                if (position==0 && i==0){
+                    this.addFirst(newData);
+                }
+                if (i==position-1){
+                    temp2 = temp.getNext();
+                    temp.setNext(new ElemOne(newData, temp2));
+                } else {
+                    temp = temp.getNext();
+                }
+            }
+        } else {
+            throw new ListException("Bad position");
+        }
     }
 
     @Override
     public int size() {
-        return list.length;
+        ElemOne<T> temp;
+        temp = first;
+        if (temp == null){
+            return 0;
+        }
+        else {
+            if (temp.getNext() == null) {
+                return 1;
+            } else {
+                int size = 1;
+                while (temp.getNext()!=null){
+                    size++;
+                    temp = temp.getNext();
+                }
+                return size;
+            }
+        }
     }
 
     @Override
-    public Object removeFirst() throws ListException {
+    public T removeFirst() throws ListException {
+        ElemOne<T> temp;
+        T deleted;
+        if (first!=null){
+            if (first.getNext()==null){
+                first = null;
+                return first.getData();
+            }else {
+                deleted = first.getData();
+                temp = first.getNext();
+                first = temp;
+                return deleted;
+            }
+        }
+        else {
+            throw new ListException("List is empty");
+        }
+    }
+
+    @Override
+    public T removeLast() throws ListException {
+        ElemOne<T> temp, temp2 = null;
+        temp = first;
+        if (first!=null){
+            if (first.getNext()==null){
+                first = null;
+                return first.getData();
+            }else {
+                while (temp.getNext() != null){
+                    temp2 = temp;
+                    temp = temp.getNext();
+                }
+                temp2.setNext(null);
+                return last.getData();
+            }
+        }
+        else {
+            throw new ListException("List is empty");
+        }
+    }
+
+    @Override
+    public T remove(int position) throws ListException {
+        ElemOne<T> temp, temp2;
+        temp = first;
+        int size = this.size();
+        if (position>=0 && position<size){
+            for (int i=0; i<size;i++){
+                if (position==0 && i==0){
+                    return this.removeFirst();
+                }
+                if (i==position-1){
+                    temp2 = temp.getNext();
+                    temp.setNext(temp2.getNext());
+                    return temp2.getData();
+                } else {
+                    temp = temp.getNext();
+                }
+            }
+        } else {
+            throw new ListException("Bad position");
+        }
         return null;
     }
 
     @Override
-    public Object removeLast() throws ListException {
-        return null;
+    public int find(T dataToFind) {
+        ElemOne<T> temp;
+        temp = first;
+        int index = 0;
+        if(temp != null){
+            while (temp != null){
+                if (temp.getData()==dataToFind){
+                    return index;
+                }
+                index++;
+                temp = temp.getNext();
+            }
+        }
+        return -1;
     }
 
     @Override
-    public Object remove(int position) throws ListException {
-        return null;
-    }
-
-    @Override
-    public int find(Object dataToFind) {
-        return 0;
-    }
-
-    @Override
-    public boolean contains(Object data) {
+    public boolean contains(T data) {
+        ElemOne<T> temp;
+        temp = first;
+        if(temp != null){
+            while (temp != null){
+                if (temp.getData()==data){
+                    return true;
+                }
+                temp = temp.getNext();
+            }
+        }
         return false;
     }
 
     @Override
     public void print() {
-        for (Object obj : list) {
-            System.out.println(obj);
+        ElemOne<T> temp;
+        temp = first;
+        if(temp != null){
+            while (temp != null){
+                System.out.println(temp.getData());
+                temp = temp.getNext();
+            }
         }
     }
 }
--- Algorytmy_i_zlożoność/Lab2/src/aiz/list/main.java
+++ Algorytmy_i_zlożoność/Lab2/src/aiz/list/main.java
@@ -1,9 +1,26 @@
 package aiz.list;
 
 public class main {
-    public static void main(String[] args){
-        ListOne objects = new ListOne();
-        objects.addFirst(new ElemOne<>("Noway"));
-        objects.print();
+    public static void main(String[] args) throws ListException {
+        ListOne listOne = new ListOne();
+        listOne.addFirst("Test1");
+        listOne.addFirst("Test2");
+        listOne.addFirst("Test3");
+        listOne.addLast(1);
+        listOne.addLast(2);
+        listOne.addLast(3);
+        listOne.addFirst("Test4");
+        listOne.addAtPosition(4, 0);
+        listOne.addAtPosition(4, 5);
+        System.out.println(listOne.removeFirst()+" - deleted");
+        System.out.println(listOne.removeFirst()+" - deleted");
+        System.out.println(listOne.removeLast()+" - deleted");
+        System.out.println(listOne.remove(4)+" - deleted");
+        System.out.println("Test3 at positions "+listOne.find("Test3"));
+        System.out.println("Test1 at positions "+listOne.find("Test1"));
+        System.out.println("4 exist ="+listOne.contains(4));
+        System.out.println("2 exist ="+listOne.contains(2));
+        System.out.println("6 exist ="+listOne.contains(6));
+        listOne.print();
     }
 }
